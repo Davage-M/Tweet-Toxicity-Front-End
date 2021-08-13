8ef9595b500884835e7fd0e3a318d134ddb3ce8a
Davage-M <davagemd@gmail.com>
2021-08-12 23:53:30 -0400

    Initial

---
+++ src/front_files/main.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import utils from './utils.js';
+import './styles.css';
+
+export default class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+
+
+    }
+    render() {
+        return (
+            <div>
+                <p>Hello World</p>
+            </div>
+        );
+    }
+}
